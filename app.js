58a7aff20ffec50af158ef290e4477409a3be889
KevinFromConn <kfc21194@gmail.com>
2021-07-13 15:54:50 -0400

    working with node to dynamically create html

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const { truncate } = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -140,5 +139,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
+  });
